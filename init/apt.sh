d63827c297f70e6331ca2a2ea191c95627870ae2
Yuichi Tanikawa <u1.tanikawa@gmail.com>
2020-03-22 23:29:38 +0900

    feat: use Linuxbrew on Ubuntu

---
+++ init/apt.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+sudo apt install \
+  colordiff \
+  fonts-ricty-diminished \
+  fzf \
+  git \
+  guake \
+  hub \
+  jq \
+  silversearcher-ag \
+  source-highlight \
+  tig \
+  tmux \
+  tree \
+  zsh \
